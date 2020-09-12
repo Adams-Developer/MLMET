7c78496872351d3d79920e5a4717401407563ed0
Tiffany Adams <tadams03@mail.roosevelt.edu>
2020-09-12 16:40:19 -0500

    ML.Net, ModelBuilder, Net Core Web Application for predicting stock quantity

---
+++ WarehouseStockPrediction/WarehouseStockPrediction/Controllers/StockPredictionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseStockPredictionML.Model;
+
+namespace WarehouseStockPrediction.Controllers
+{
+    public class StockPredictionController : Controller
+    {
+       [HttpGet]
+       public IActionResult StockPrediction()
+       {
+            return View();
+       }
+
+        [HttpPost]
+        public ActionResult StockPrediction(ModelInput modelInput)
+        {
+            ViewBag.Result = "";
+
+            var stockPredictions = ConsumeModel.Predict(modelInput);
+            ViewBag.Result = stockPredictions;
+
+            ViewData["ItemId"] = modelInput.ItemId;
+            ViewData["LocationCode"] = modelInput.LocationCode;
+
+            return View();
+
+        }
+
+    }
+}
